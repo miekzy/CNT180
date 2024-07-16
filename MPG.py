5b88501842f551a0f838073f4bc95b267389c090
miekzy <Justakid065@gmail.com>
2024-07-15 20:53:15 -0700

    MPG.py

--- MPG.py
+++ MPG.py
@@ -1,4 +1,5 @@
 miles_driven = float(input("miles driven:"))
 gallons_used = float(input('Gallons Used:'))
-MPG = gallons_used / miles_driven
+MPG = miles_driven / gallons_used
+MPG = round(MPG, 2)
 print(f"MPG: {MPG}")
