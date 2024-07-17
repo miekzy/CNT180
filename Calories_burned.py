f6ab76ab7b84c99e86a2c138cd02f2d3b89590d0
miekzy <Justakid065@gmail.com>
2024-07-16 19:23:37 -0700

    Create Calories_burned.py

---
+++ Calories_burned.py
@@ -0,0 +1,5 @@
+calories_per_minute = 4.2
+
+for minutes in [10, 15, 20, 25, 30]:
+    calories_burned = minutes * calories_per_minute
+    print(f"Calories burned after {minutes} minutes: {calories_burned:.2f}")
