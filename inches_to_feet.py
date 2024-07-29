b75019c263d76e74cf9a4462fabe1fabef8c64a3
miekzy <Justakid065@gmail.com>
2024-07-29 12:45:20 -0700

    Update inches_to_feet.py

--- inches_to_feet.py
+++ inches_to_feet.py
@@ -1,13 +1,4 @@
 def feet_to_inches(feet):
     return feet * 12
 
-def main():
-    try:
-        feet = float(input("Enter number of feet: "))
-        inches = feet_to_inches(feet)
-        print(f"{feet} feet is equal to {inches} inches.")
-    except ValueError:
-        print("Please enter a valid number.")
-
-if __name__ == "__main__":
-    main()
+print(feet_to_inches(12))
