f5dd0fb04b9cdd6fa611f7e8bb51208fac5d107f
miekzy <Justakid065@gmail.com>
2024-07-31 00:17:59 -0700

    Create Min&Max.py

---
+++ Min&Max.py
@@ -0,0 +1,5 @@
+listA = [23, 12, 45, 22, 11, 78, 13, 90, 45, 88, 10, -5, -1]
+max_number = max(listA)
+min_number = min(listA)
+print("Maximum number:", max_number)
+print("Minimum number:", min_number)
